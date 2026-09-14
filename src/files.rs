eccb9c5f27ff952450a16644c1cdc55ce3fab483
Arseniy <arsyhiy32@gmail.com>
2026-09-14 19:34:01 +0300

    adding prototype

---
+++ src/files.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+use std::fs;
+use std::path::PathBuf;
+use std::env;
+
+fn get_path() -> PathBuf {
+    let mut path = PathBuf::from(
+        env::var("HOME").expect("HOME не найден")
+    );
+
+    path.push("sites.txt");
+
+    path
+}
+
+pub fn load_sites() -> HashMap<String, String> {
+
+    let content = fs::read_to_string(get_path())
+        .expect("Не удалось прочитать файл");
+
+    let mut sites = HashMap::new();
+
+    for line in content.lines() {
+        let mut parts = line.split_whitespace();
+
+        let name = match parts.next() {
+            Some(value) => value,
+            None => continue,
+        };
+
+        let url = match parts.next() {
+            Some(value) => value,
+            None => continue,
+        };
+
+        sites.insert(name.to_string(), url.to_string());
+    }
+
+    sites
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,50 @@
-fn main() {
-    let url = "https://youtube.com";
+use std::collections::HashMap;
 
-    match reqwest::blocking::get(url) {
-        Ok(response) => {
-            println!("HTTP доступ есть: {}", response.status());
-        }
-        Err(e) => {
-            println!("HTTP доступа нет: {}", e);
-        }
+mod files;
+
+fn get_connection_status(sites: HashMap<String, String>,) -> HashMap<String, String> {
+    let mut results = HashMap::new();
+
+    for (name, url) in &sites {
+
+        let status = match reqwest::blocking::get(url) {
+            Ok(response) => {
+                response.status().to_string()
+            }
+            Err(e) => {
+                format!("Ошибка: {:?}", e)
+            }
+        };
+
+    results.insert(name.clone(), status);
+}
+
+    results
+}
+
+fn interface(sites: HashMap<String, String>) {
+    for (name, status) in &sites {
+        println!("{} -> {}", name, status);
     }
+}
+
+
+fn main() {
+
+    let sites = files::load_sites();
+
+    // get list of site
+    // move it in array or something
+    // via loop get array of site name andresponses like name and key 
+    //
+    // let url = env::args()
+    //     .nth(1)
+    //     .expect("Укажите URL");
+    //
+    // let status = get_connection_status(&url);
+    
+
+    let site_n_status = get_connection_status(sites);
+    interface(site_n_status);
+    
 }
